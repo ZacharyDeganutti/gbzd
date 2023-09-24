6f3739e9bfedf900cb5cb602de2f960ea19f276a
Zachary Deganutti <zjdeganutti@gmail.com>
2023-09-24 04:00:53 -0400

    Moving stuff around pt. 1

---
+++ src/cpu_execute.rs
@@ -0,0 +1,12 @@
+use crate::memory_gb::Address;
+use crate::memory_gb::Byte;
+use crate::memory_gb::Signed;
+use crate::memory_gb::Word;
+use crate::cpu::*;
+use crate::cpu_ops;
+
+impl Cpu {
+    pub fn step(&mut self) {
+        
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 mod cpu;
 mod memory_gb;
+mod cpu_ops;
+mod cpu_execute;
 
 use std::rc::Rc;
 use std::cell::RefCell;
+use crate::cpu::*;
 
 fn main() {
     let system_memory = Rc::new(RefCell::new(memory_gb::MemoryMap::new()));
-    let mut cpu = cpu::Cpu::new(system_memory.clone());
-    cpu.ld_byte_op(
-        cpu::ByteRegister::new(cpu::ByteRegisterName::RegA),
-        cpu::ByteRegister::new(cpu::ByteRegisterName::RegE)
+    let mut cpu = Cpu::new(system_memory.clone());
+    cpu.ld_byte(
+        ByteRegister::new(ByteRegisterName::RegA),
+        ByteRegister::new(ByteRegisterName::RegE)
     );
     println!("{}", cpu.registers.a);
-    cpu.ld_byte_op(
-        cpu::ByteRegister::new(cpu::ByteRegisterName::RegH),
-        cpu::ByteImmediate::new(128),
+    cpu.ld_byte(
+        ByteRegister::new(ByteRegisterName::RegH),
+        ByteImmediate::new(128),
     );
-    cpu.inc_byte_op(cpu::ByteRegister::new(cpu::ByteRegisterName::RegH));
+    cpu.inc_byte(ByteRegister::new(ByteRegisterName::RegH));
     println!("{}", cpu.registers.h);
 }
